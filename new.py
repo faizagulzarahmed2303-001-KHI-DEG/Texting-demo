a92a6b38e3c187788c126ae05fbd0c1e7ab49c01
faizagulzarahmed2303-001-KHI-DEG <faiza.gulzar@xloopdigital.com>
2023-02-09 12:23:47 +0500

    another program in python
    I am using create a python program

--- new.py
+++ new.py
@@ -16,3 +16,19 @@ my_function_with_args("Faiza", "a great year!")
 
 # after this line x will hold the value 3!
 x = sum_two_numbers(1,2)
+
+
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return []
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return ""
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
